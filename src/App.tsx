cc3136dc7a04bbe5d26c7e113d1fc88033daff6b
Lílian Vieira <lilianv528@gmail.com>
2025-08-17 17:26:22 -0300

    renderizaçao dos filmes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Banner from "./components/Banner"
 import Header from "./components/Header";
+import MovieSection from "./components/MovieSection";
 
 function App() {
   return (
     <>
     <Header />
-      <Banner src="./public/Imagens/banner.png" alt="Banner" />
+    <Banner src="./public/Imagens/banner.png" alt="Banner" />
+    <MovieSection />
     </>
     
   )
